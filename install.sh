ce14d0b5fc1867d1b59eafcc31d9b906f5f2e161
haze <jobb.avakian@gmail.com>
2023-02-18 16:15:59 +0100

    Added oh-my-zsh and powerlevel10k in the installation

--- install.sh
+++ install.sh
@@ -21,14 +21,19 @@ fi
 # Clone TPM (Tmux package manager)
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-
 # Clone dotfiles repository
 git clone --recurse-submodules https://github.com/Its-Haze/dotfiles.git ~/dotfiles
+
+# Install Oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install Powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # Copy configuration files
 cp ~/dotfiles/zsh/.zshrc ~/.zshrc
 #cp ~/dotfiles/zsh/custom.zsh ~/.oh-my-zsh/custom/custom.zsh
-cp ~/dotfiles/oh-my-zsh/themes/custom.zsh-theme ~/.oh-my-zsh/custom/themes/
+#cp ~/dotfiles/oh-my-zsh/themes/custom.zsh-theme ~/.oh-my-zsh/custom/themes/
 cp ~/dotfiles/powerlevel10k/.p10k.zsh ~/.p10k.zsh
 #cp ~/dotfiles/oh-my-zsh/plugins/plugin1/plugin1.zsh ~/.oh-my-zsh/custom/plugins/plugin 1/
 #cp ~/dotfiles/oh-my-zsh/plugins/plugin2/plugin2.zsh ~/.oh-my-zsh/custom/plugins/plugin2/
@@ -36,4 +41,7 @@ cp ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
 mkdir -p ~/.config/nvim/
 cp ~/dotfiles/kickstart.nvim/init.vim ~/.config/nvim/
 #cp ~/dotfiles/nvim/coc-settings.json ~/.config/nvim/
+
+# Set the ZSH-THEME to powerlevel10k
+sed -i 's/ZSH_THEME=".*"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' ~/.zshrc
 
